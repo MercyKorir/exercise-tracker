1b6a8d5c4132c78780e90a104c57e14d1f6f40ae
MercyKorir <mcthegreat78@gmail.com>
2023-11-18 14:20:37 +0300

    feat: complete all CRUD controllers and routes for exercises

--- server/controllers/exerciseController.js
+++ server/controllers/exerciseController.js
@@ -36,3 +36,43 @@ export const createExercise = async (req, res) => {
     res.status(500).json({ message: 'Error creating exercise' });
   }
 };
+
+// Get exercise by id
+export const getExerciseById = async (req, res) => {
+  try {
+    const exercise = await Exercise.findById(req.params.id);
+    res.json(exercise);
+  } catch (error) {
+    console.error('Error getting exercise', error);
+    res.status(500).json({ message: 'Error getting exercise' });
+  }
+};
+
+// Delete exercise by id
+export const deleteExerciseById = async (req, res) => {
+  try {
+    await Exercise.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Exercise deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting exercise', error);
+    res.status(500).json({ message: 'Error deleting exercise' });
+  }
+};
+
+// Update exercise by id using PATCH meaning only update the fields that are passed in
+export const updateExerciseById = async (req, res) => {
+  try {
+    const exercise = await Exercise.findById(req.params.id);
+    const { username, description, duration } = req.body;
+    const date = Date.parse(req.body.date);
+    if (username) exercise.username = username;
+    if (description) exercise.description = description;
+    if (duration) exercise.duration = duration;
+    if (date) exercise.date = date;
+    await exercise.save();
+    res.json({ message: 'Exercise updated successfully', exercise });
+  } catch (error) {
+    console.error('Error updating exercise', error);
+    res.status(500).json({ message: 'Error updating exercise' });
+  }
+};
--- server/routes/exercise.js
+++ server/routes/exercise.js
@@ -2,6 +2,9 @@ import express from 'express';
 import {
   getExercises,
   createExercise,
+  getExerciseById,
+  deleteExerciseById,
+  updateExerciseById,
 } from '../controllers/exerciseController.js';
 
 const router = express.Router();
@@ -9,5 +12,11 @@ const router = express.Router();
 router.get('/', getExercises);
 
 router.post('/add', createExercise);
+
+router.get('/:id', getExerciseById);
+
+router.delete('/:id', deleteExerciseById);
+
+router.patch('/:id', updateExerciseById);
 
 export default router;
